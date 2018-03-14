00f35fc7b0e54bf40ce7d757716aa10060b0ec68
thureson <tom.thureson@gmail.com>
2018-03-14 20:36:56 +0200

    Implemented a route for single product.

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,19 +1,41 @@
 const express = require('express');
 const router = express.Router();
-const userStore = require('../db/userStore');
+const productStore = require('../db/productStore');
 
 router.get('/', async (req, res) => {
-    const products = await productStore.getProducts();
-    if(products) {
-      res.status(200).json({
-        products: products
-      });
-    } else {
-      res.status(500).json({
-        error_code: 'internal_error',
-        message: 'Internal error'
-      });
+    try {
+        const products = await productStore.getProducts();
+        if(products) {
+          res.status(200).json({
+            products: products
+          });
+        } else {
+          res.status(500).json({
+            error_code: 'internal_error',
+            message: 'Internal error'
+          });
+        }
+    } catch (exception) {
+        response.status(500).send({ error: 'Database communication error' })
     }
 });
+
+router.get('/:barcode', async (req, res) => {
+        try {
+            const product = await productStore.findByBarcode(req.params.barcode)  
+            if(product) {
+              res.status(200).json({
+                product: product
+              });
+            } else {
+              res.status(500).json({
+                error_code: 'internal_error',
+                message: 'Internal error'
+              });
+            }
+        } catch (exception) {
+            res.status(400).send({ error: 'malformatted id' })
+        }
+})
 
 module.exports = router;
